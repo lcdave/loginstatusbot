e1efaed86ce00ec7c28dbe4eaa456cffda3bce8c
Davide Lo Cascio <davide@4eyes.ch>
2019-08-24 22:10:18 +0200

    implements chat id fix

--- bot.js
+++ bot.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
-const token = process.env.TOKEN;
+const token = '804043319:AAHus8vLZnxwHiEVCnwz5sm9nLLhH1JCpJ0';
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/login/, (msg) => {
 	bot.sendMessage(msg.chat.id, '❌❌❌❌' + msg.from.first_name + ' IS NOW LOGGED IN! ❌❌❌❌').then((m) => {
-		bot.pinChatMessage(-1001448364430, m.message_id);
+		bot.pinChatMessage(m.chat.id, m.message_id);
+		console.log(m);
 	});
 });
 
