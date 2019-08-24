4482ec3bb0cc46bdaf40f9acb71a9e4d417ee319
Davide Lo Cascio <davide@4eyes.ch>
2019-08-24 20:45:57 +0200

    implements structure for deployment to heroku

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-var fs = require('fs');
-const token = fs.readFileSync('./A15666_t650641.txt').toString().split("\n");
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(token, {polling: true});
-
-bot.onText(/\/login/, (msg) => {
-	bot.sendMessage(msg.chat.id, '❌❌❌❌' + msg.from.first_name + ' IS NOW LOGGED IN! ❌❌❌❌').then((m) => {
-		bot.pinChatMessage(-1001448364430, m.message_id);
-	});
-});
-
-bot.onText(/\/logout/, (msg) => {
-	bot.sendMessage(msg.chat.id, '✅✅✅✅ NOBODY IS PLAYING RIGHT NOW ✅✅✅✅').then((m) => {
-		bot.pinChatMessage(-1001448364430, m.message_id);
-	});
-});
+require('./bot');
+require('./web');
